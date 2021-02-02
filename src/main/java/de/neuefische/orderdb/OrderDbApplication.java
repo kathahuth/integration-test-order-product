c4e424c7b575c208fdc133b9cb5e77920621a9bc
Tom Grehl <grehl.tom@googlemail.com>
2021-02-02 10:14:05 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/orderdb/OrderDbApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/orderdb/OrderDbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
